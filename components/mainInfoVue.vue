45508bb62c4e3c9c6882366c5322008363862a46
Dmytro <alex.kykylkan@gmail.com>
2023-12-19 11:53:13 +0200

    feature: age calculation

--- components/mainInfoVue.vue
+++ components/mainInfoVue.vue
@@ -1,3 +1,25 @@
 <template>
   <h1 class="text-3xl font-medium uppercase">Коновалюк Дмитро Сергійович</h1>
+  <div class="mt-2 flex">
+    <div class="mr-6 flex items-center">
+      <Icon name="ic:outline-place" class="mr-1" />
+      <p>Україна, м. Черкаси</p>
+    </div>
+    <div class="flex items-center">
+      <Icon name="material-symbols:info-outline" class="mr-1" />
+      <p>{{ age }} років</p>
+    </div>
+  </div>
 </template>
+
+<script setup lang="ts">
+const calculateAge = (dateOfBirth: Date): Number => {
+  const now = new Date();
+  const diff = Math.abs(now.getTime() - dateOfBirth.getTime());
+  const age = Math.floor(diff / (1000 * 60 * 60 * 24 * 365));
+
+  return age;
+};
+
+const age = calculateAge(new Date("1994-03-08"));
+</script>
