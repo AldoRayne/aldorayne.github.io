99fab91590833c02d0962d2c1b7f637f7c768bb8
Dmytro <alex.kykylkan@gmail.com>
2023-12-25 14:14:54 +0200

    text change

--- components/mainInfoVue.vue
+++ components/mainInfoVue.vue
@@ -1,11 +1,11 @@
 <template>
   <section>
     <h1 class="text-3xl font-medium uppercase">Коновалюк Дмитро Сергійович</h1>
-    <p class="mt-1 text-lg font-medium uppercase">Front-end розробник</p>
+    <p class="mt-1 text-lg font-medium uppercase">Front-end розробник (офіс / віддалено)</p>
     <div class="mt-2 flex">
       <div class="mr-6 flex items-center">
         <Icon name="ic:outline-place" class="mr-1" />
-        <p>Україна, м. Черкаси (офіс / віддалено)</p>
+        <p>Україна, м. Черкаси</p>
       </div>
       <div class="flex items-center">
         <Icon name="openmoji:man-mage" class="mr-1" />
